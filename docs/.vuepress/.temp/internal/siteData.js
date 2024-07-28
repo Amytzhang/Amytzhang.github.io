b397827ef1f1e7ea0bcc6d7b3febf28ac463dd56
Amytzhang <2429323352@qq.com>
2024-07-28 22:57:05 +0800

    add:init

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/amyrecord/\",\"lang\":\"en-US\",\"title\":\"我的笔记\",\"description\":\"My first VuePress Site\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"en-US\",\"title\":\"我的笔记\",\"description\":\"My first VuePress Site\",\"head\":[],\"locales\":{}}")
